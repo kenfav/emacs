4a39bd148216e40d937ebe21921eabc31f8f35fb
kenfav <kenfavero@gmail.com>
2022-04-20 02:16:36 +0000

    my config

---
+++ elpa/rebecca-theme-20180324.821/rebecca-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from rebecca-theme.el  -*- no-byte-compile: t -*-
+(define-package "rebecca-theme" "20180324.821" "Rebecca Purple Theme" '((emacs "24")) :commit "4b8b5aae9099185e07c2b4cac4943c7f66a3f003" :authors '(("vic" . "vborja@apache.org")) :maintainer '("vic" . "vborja@apache.org") :keywords '("theme" "dark") :url "https://github.com/vic/rebecca-theme")
---
+++ elpa/term+mux-20140211.749/term+mux-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from term+mux.el  -*- no-byte-compile: t -*-
+(define-package "term+mux" "20140211.749" "term+ terminal multiplexer and session management" '((term+ "0.1") (tab-group "0.1")) :commit "81b60e80cf008472bfd7fad9233af2ef722c208a" :authors '(("INA Lintaro <tarao.gnn at gmail.com>")) :maintainer '("INA Lintaro <tarao.gnn at gmail.com>") :keywords '("terminal" "emulation") :url "http://github.com/tarao/term+-el")
